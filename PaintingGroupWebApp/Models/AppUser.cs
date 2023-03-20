a81bd6bcc188e323f3cacb69097cc2871edbdf98
diaconescuaurelian <aurelian.diaconescu05@gmail.com>
2023-03-20 18:48:25 +0200

    Solved some errors regarding the models and the enums

--- PaintingGroupWebApp/Models/AppUser.cs
+++ PaintingGroupWebApp/Models/AppUser.cs
@@ -5,7 +5,7 @@
         public string? FavouriteStyle { get; set; }
         public string? FavouriteMedium { get; set; }
         public Address? MAddress { get; set; }
-        public ICollection<CLub> Clubs { get; set; }
+        public ICollection<Club> Clubs { get; set; }
         public ICollection<Meeting> Meetings { get; set; }
 
     }
